8e57637a1c5007daec9df8bd1e7bfcd800eb1068
Ali <net_lina95@mail.ru>
2023-07-08 20:04:31 +0300

    дз готово!

---
+++ 02/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (a<b)
+    max = b;
+if (max<c)
+    max = c;
+
+Console.WriteLine(max);
